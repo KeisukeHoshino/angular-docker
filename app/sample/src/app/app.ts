54768d81fbac52fdfb171ae6a9e2d20a98f46d88
KeisukeHoshino <Keisuke.hoshino.pl@gmail.com>
2025-07-27 15:34:30 +0900

    Homeコンポーネントを追加

--- app/sample/src/app/app.ts
+++ app/sample/src/app/app.ts
@@ -1,11 +1,30 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [
+    // RouterOutlet,
+    Home,
+  ],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  // templateUrl: './app.html',
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('sample');
