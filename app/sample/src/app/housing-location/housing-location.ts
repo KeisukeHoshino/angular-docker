40fceba792e06db588f6227a6f283e9a4ae4eb75
KeisukeHoshino <Keisuke.hoshino.pl@gmail.com>
2025-07-27 16:37:22 +0900

    コンポーネントのテンプレートに補間を追加する

--- app/sample/src/app/housing-location/housing-location.ts
+++ app/sample/src/app/housing-location/housing-location.ts
@@ -1,15 +1,24 @@
 import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../housinglocation';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+    </section>
   `,
-  styleUrl: './housing-location.css'
+  styleUrl: './housing-location.css',
 })
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
