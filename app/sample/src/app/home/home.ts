21c66a0b68b9170fd662fb790896cfea950699a0
KeisukeHoshino <Keisuke.hoshino.pl@gmail.com>
2025-07-27 15:56:12 +0900

    housing-locationコンポーネントの追加

--- app/sample/src/app/home/home.ts
+++ app/sample/src/app/home/home.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from '../housing-location/housing-location';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HousingLocation],
   template: `
     <section>
-    <form>
-      <input type="text" name="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
-    </form>
-  </section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
+    </section>
   `,
-  styleUrl: './home.css'
+  styleUrl: './home.css',
 })
-export class Home {
-
-}
+export class Home {}
